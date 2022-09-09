a30e8fc77d93897e6235a829fe9e4107cf7e5369
Gun Khemnak <gunnn_boy_hand@hotmail.com>
2022-09-09 13:02:47 +0700

    edit sctipt

--- logs.sh
+++ logs.sh
@@ -1,21 +1,17 @@
 if [ -z "$1" ]; then
-    echo "Argument <connect|api|basic|nginx> is required!!!"
+    echo "Argument <minifi|api|nginx> is required!!!"
     exit 1
 fi
 
 MODE=$1
 
-if [[ $MODE =~ ^(connect)$ ]]; 
+if [[ $MODE =~ ^(minifi)$ ]]; 
 then
-     docker logs connect -f --tail 100
+     docker logs minifi -f --tail 100
 fi
 if [[ $MODE =~ ^(api)$ ]]; 
 then
      docker logs hisgateway-api -f --tail 100
-fi
-if [[ $MODE =~ ^(basic)$ ]]; 
-then
-     docker logs history-api -f --tail 100
 fi
 if [[ $MODE =~ ^(nginx)$ ]]; 
 then
